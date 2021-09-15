7f119b739c12bd79644cce645f9ae8d2f4275f3c
Pavel Prochazka <pavel.prochazka@ceitec.vutbr.cz>
2021-09-15 21:42:37 +0200

    Reverse word implemented

---
+++ VS-git-test/ReverseWord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_git_test
+{
+    public static class ReverseWord
+    {
+        static ReverseWord()
+        {
+            Console.WriteLine("Not implemented static constructor");
+        }
+    }
+}
