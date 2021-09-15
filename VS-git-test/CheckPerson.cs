ccc80e42046abc5ee25ee60dc058df7600da8dbc
Pavel Prochazka <pavel.prochazka@ceitec.vutbr.cz>
2021-09-15 21:50:12 +0200

    Person check class created and implemented

---
+++ VS-git-test/CheckPerson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_git_test
+{
+    class CheckPerson
+    {
+        public CheckPerson()
+        {
+            Console.WriteLine("Checking the person ...");
+        }
+    }
+}
